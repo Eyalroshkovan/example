a02972b6f2e66c184cea6345e93d9f4f983560ef
eyal_roshkovan <eyalroshkovan12@gmail.com>
2024-09-03 23:59:02 +0300

    added code

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,10 +1,7 @@
-from fastapi import FastAPI, Response
+import asyncio
+
 from playwright.async_api import async_playwright
-import os
 
-app = FastAPI()
-
-@app.get("/")
 async def root():
     async with async_playwright() as p:
         browser = await p.chromium.connect("wss://chrome.browserless.io?token=QmcNq8L5Wa9jMu14c85e16fff9236811c34a5767a8")
@@ -15,8 +12,7 @@ async def root():
         await page.set_viewport_size({'width': 1920, 'height': 1080})
 
         await page.goto('https://example.com')
-        screenshot_bytes = await page.screenshot()
 
         await browser.close()
 
-        return Response(content=screenshot_bytes, media_type='image/png')
+asyncio.run(root())
